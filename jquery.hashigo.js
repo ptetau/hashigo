2d3eb01621a207517210d90d3cb125319bd17045
Patrick Te Tau <patmania@gmail.com>
2010-05-07 01:19:09 +1200

    Added high speed calc function, improved code organisation

--- jquery.hashigo.js
+++ jquery.hashigo.js
@@ -1,160 +1,183 @@
+jQuery.fn.calc = function() {
 
-$.fn.calc = function(formula,options){
-    
-    var FT = $.fn.calc.FormulaTool;
-    
-	var defaults = {triggerOnLoad:true,
-					event:'keyup'};
-	
-	if	(!!options){				
-		$.extend(options,defaults);
-	}else{
-	    options = defaults;
-	}
-	
-	var $element = $(this);
-	
-	var selectors = FT.getSelectors(formula);
-	
-	if (selectors.length === 0) {
-		return $(this);
-	}
+    //calc seamlessly handles evaluated formulas or functions 
+    //with $valiableElements. It needs to know which though.
+    var CalcTypes = {
+        FORMULA: 'formula',
+        //Formulas are eval'd
+        FUNCTION: 'function'
+    };
 
-	var fw = FT.formulaWrapper($element,formula);
-	
-	$(selectors.join(',')).bind(options.event,fw);
-	
-	if(defaults.triggerOnLoad){
-		fw();
-	}
-	
-	return $(this);
-}
+    //formula or function
+    var type = '';
 
-$.fn.calcHS = function(funktion,$elements,options){
-    
-    var FT = $.fn.calc.FormulaTool;
-    
-	var defaults = {triggerOnLoad:true,
-					event:'keyup'};
-	
-	if	(!!options){				
-		$.extend(options,defaults);
-	}else{
-	    options = defaults;
-	}
-	
-	var $element = $(this);
-	
-	if ($elements.length === 0) {
-		return $(this);
-	}
+    //for use with functions
+    var $variableElements = [];
 
-	var fw = FT.functionWrapper($element,funktion);
-	
-	console.log($elements);
-	_.each($elements,function($element){$element.bind(options.event,fw)});
-	console.log($elements);
-	if(defaults.triggerOnLoad){
-		fw();
-	}
-	
-	return $(this);
-}
+    //user defined functions
+    var options = {};
 
-$.fn.calc.FormulaTool = (function(){
-        function getValueFromInput($element){
-    	
-		    if ($element.is('input')){
-			    var val = $element.val();
-    			
-			    if (val === ''){
-				    return '""';
-			    }
-    			
-			    if (_.isNumber(val)){
-				    return parseFloat(val);
-			    }
-			    if (_.isString(val)){
-				    return '"' + escape(val) + '"';
-			    }	
-    			
-			    return '""';
-		    }
-    		
-            var $closestInput = $element.find('input');
+    //the function if there is one
+    var funktion = null;
 
-            if ($closestInput.length === 0){ 
-                throw 'DOM Element (id : ' + $element.attr('id') + ', classes : ' + $element.attr('class') + ') is not or does contain a valid input';
+    //the formula if there is one
+    var formuka = null;
+
+    //determine what type of calculation we have
+    if (_(arguments[0]).isString()) {
+
+        //formulas are strings
+        type = CalcTypes.FORMULA;
+        formula = arguments[0];
+
+        //use options if provided
+        options = !!arguments[1] ? arguments[1] : options;
+
+    } else if (_(arguments[0]).isFunction()) {
+
+        type = CalcTypes.FUNCTION;
+        funktion = arguments[0];
+
+        if (_(arguments[1]).isArray()) {
+
+            //function has $variableElements
+            $variableElements = arguments[1];
+
+            //use options if provided
+            options = !!arguments[2] ? arguments[2] : options;
+        }
+    }
+
+    //a shortcut to our formula tools
+    var FT = jQuery.fn.calc.FormulaTool;
+
+    var settings = jQuery.extend(jQuery.fn.calc.settings, options);
+
+    jQuery(this).each(function() {
+        var $element = jQuery(this);
+
+        var fw = null;
+
+        switch (type) {
+        case CalcTypes.FUNCTION:
+
+            fw = FT.functionWrapper($element, funktion, $variableElements);
+
+            //Assign event handlers on variable elements
+            _.each($variableElements, function($variableElement) {
+                $variableElement.bind(settings.event, fw)
+                });
+
+            break;
+
+        case CalcTypes.FORMULA:
+
+            fw = FT.formulaWrapper($element, formula);
+
+            var selectors = FT.getSelectors(formula);
+
+            //Assign event handlers on variable elements
+            jQuery(selectors.join(',')).bind(settings.event, fw);
+
+            break;
+
+        default:
+            //Do nothing
             }
 
-            return getValueFromInput($closestInput);	
-	    }
-	    
-        function replaceSelectorsWithValues(formula){
-		    var selectors = getSelectors(formula);
-    		
-		    if (selectors.length === 0) {
-			    return formula;
-		    }
-    		
-		    var outputFormula = formula;
-    		
-		    _(selectors).each(function(selector){
-			    outputFormula = replaceSelectorWithValue(outputFormula,selector);
-		    });
-    		
-		    return outputFormula;
-	    }
+        if (settings.triggerOnLoad) {
+            fw();
+        }
+    });
 
-	    function replaceSelectorWithValue(formula,selector){
-		    return formula.replace('${' + selector + '}',getValueFromInput($(selector)));
-	    }
+    return jQuery(this);
+}
 
-	    function evaluateFormula(formula){
-		    var js = replaceSelectorsWithValues(formula);
-		    return eval(js);
-	    }
+jQuery.fn.calc.settings = {
+    event: 'keyup',
+    triggerOnLoad: true
+}
 
-	    function getSelectors(formula){
-		    return _(formula.match(/\$\{(.*?)\}/g)).map(function(sigil){
-			    return sigil.match(/\$\{(.*?)\}/)[1];
-		    });
-	    }
-	    
-	    function formulaWrapper($element,formula){
-		    return function(){
-			    if ($element.is('input')){
-				    $element.val(evaluateFormula(formula));
-			    }else{
-				    $element.text(unescape(evaluateFormula(formula)));
-			    };
-		    }
-	    }
-	    
-	    function functionWrapper($element,funktion,elements){
-	        return function(){
-	        
-	            //Map the array of elements to their corresponding values
-	            var values = _(elements).map(function(element){
-	                return getValueFromInput($(element));
-	            });
-	            
-	            //Apply these values as arguments on the supplied funktion    
-	            if ($element.is('input')){
-				    $element.val(funktion.apply(this,values));
-			    }else{
-				    $element.text(unescape(funktion.apply(this,values)));
-			    };
-	        } 
-	    }
-	    
-	    return{
-	        getSelectors : getSelectors,
-	        evaluateFormula : evaluateFormula,
-	        evaluateSelectors : replaceSelectorsWithValues,
-	        formulaWrapper : formulaWrapper,
-	        functionWrapper : functionWrapper 
-	    }
+jQuery.fn.calc.FormulaTool = (function() {
+    function getInputValue($element) {
+
+        if ($element.is('input')) {
+            var val = $element.val();
+            return escape(val);
+        }
+
+        var $closestInput = $element.find('input');
+
+        if ($closestInput.length === 0) {
+            throw 'DOM Element (id : ' + $element.attr('id') + ', classes : ' + $element.attr('class') + ') is not or does contain a valid input';
+        }
+
+        return getInputValue($closestInput);
+    }
+
+    function getResolvedFormula(formula) {
+        var selectors = getSelectors(formula);
+
+        if (selectors.length === 0) {
+            return formula;
+        }
+
+        var outputFormula = formula;
+
+        _(selectors).each(function(selector) {
+            outputFormula = resolveSelectorAsValue(outputFormula, selector);
+        });
+
+        return outputFormula;
+    }
+
+    function resolveSelectorAsValue(formula, selector) {
+        return formula.replace('${' + selector + '}', '"' + getInputValue(jQuery(selector)) + '"');
+    }
+
+    function resolveAndEvalFormula(formula) {
+        var js = getResolvedFormula(formula);
+        return eval(js);
+    }
+
+    function getSelectors(formula) {
+        return _(formula.match(/\$\{(.*?)\}/g)).map(function(sigil) {
+            return sigil.match(/\$\{(.*?)\}/)[1];
+        });
+    }
+
+    function setContent($element, content) {
+        if ($element.is('input')) {
+            $element.val(content);
+        } else {
+            $element.text(content);
+        };
+    }
+
+    function formulaWrapper($element, formula) {
+        return function() {
+            setContent($element, unescape(resolveAndEvalFormula(formula)));
+        }
+    }
+
+    function functionWrapper($element, funktion, $variableElements) {
+        return function() {
+
+            //Map the array of elements to their corresponding values
+            var values = _($variableElements).map(function($element) {
+                return getInputValue($element);
+            });
+
+            //Apply these values as arguments on the supplied funktion    
+            setContent($element, unescape(funktion.apply(this, values)));
+        }
+    }
+
+    return {
+        getSelectors: getSelectors,
+        resolveAndEvalFormula: resolveAndEvalFormula,
+        getResolvedFormula: getResolvedFormula,
+        formulaWrapper: formulaWrapper,
+        functionWrapper: functionWrapper
+    }
 })();
-
