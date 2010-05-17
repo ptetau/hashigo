1279eac4e3470a884ddad10831148549e6ea445a
Patrick Te Tau <patmania@gmail.com>
2010-05-18 00:55:48 +1200

    * Highspeed formula resolution * Code reorganisation * Parsed through JSlint and JSBeautifier

--- jquery.hashigo.js
+++ jquery.hashigo.js
@@ -1,12 +1,22 @@
-jQuery.fn.calc = function(functionOrFormula,elementArrayOrOptions,optionsOrUndefined) {
-		
+/*!
+ * jQuery.hashigo.js
+ *
+ * Requires jQuery 1.4+ (http://jquery.com) 
+ * and Underscore.js 1.0+ (http://documentcloud.github.com/underscore/)
+ *
+ * Copyright 2010, Patrick Te Tau
+ * Licensed under the MIT license.
+ *
+ */
+ 
+jQuery.fn.calc = function (functionOrFormula, elementArrayOrOptions, optionsOrUndefined) {
+
     //calc seamlessly handles evaluated formulas or functions 
     //with $valiableElements. The arguments will be of different
     //types depending on invocation
-
     var CalcTypes = {
-        FORMULA: 'formula', // Arguments = 	calc(formula[,options])
-        FUNCTION: 'function'// 				calc(function[,values[], options])
+        FORMULA: 'formula',  // calc(formula[,options])
+        FUNCTION: 'function' // calc(function[,values[], options])
     };
 
     //formula or function
@@ -32,7 +42,7 @@ jQuery.fn.calc = function(functionOrFormula,elementArrayOrOptions,optionsOrUndef
         formula = functionOrFormula;
 
         //use options if provided
-        options = !!functionOrFormula ? elementArrayOrOptions : options;
+        options = !! functionOrFormula ? elementArrayOrOptions : options;
 
     } else if (_(functionOrFormula).isFunction()) {
 
@@ -45,33 +55,33 @@ jQuery.fn.calc = function(functionOrFormula,elementArrayOrOptions,optionsOrUndef
             elementsArr = elementArrayOrOptions;
 
             //use options if provided
-            options = !!optionsOrUndefined ? optionsOrUndefined : options;
+            options = !! optionsOrUndefined ? optionsOrUndefined : options;
         }
     }
 
     //functionOrFormula shortcut to our formula tools
     var FT = jQuery.fn.calc.FormulaTool;
 
-    var settings = jQuery.extend({},jQuery.fn.calc.Settings, options);
+    var settings = jQuery.extend({}, jQuery.fn.calc.Settings, options);
 
-	var $this = jQuery(this);
-    
-    $this.each(function() {
+    var $this = jQuery(this);
+
+    $this.each(function () {
         var $element = $this;
 
         var fw = null;
 
         switch (type) {
         case CalcTypes.FUNCTION:
-        		
+
             fw = FT.functionWrapper($element, funktion, elementsArr);
 
             //Assign event handlers on variable elements
             //but only assign each handler once!
-			_(elementsArr).chain().uniq().each(function($element) {
+            _(elementsArr).chain().uniq().each(function ($element) {
                 $element.bind(settings.event, fw);
             });
-			
+
             break;
 
         case CalcTypes.FORMULA:
@@ -94,7 +104,7 @@ jQuery.fn.calc = function(functionOrFormula,elementArrayOrOptions,optionsOrUndef
             fw();
         }
     });
-	
+
     return $this;
 };
 
@@ -103,105 +113,87 @@ jQuery.fn.calc.Settings = {
     triggerOnLoad: true
 };
 
-jQuery.fn.calc.FormulaTool = (function() {
-    function resolve(elements) {
-    	
-		var values = _(elements).map(function(element){
-			var $element = $(element)
-			
-			var value = null;
-			
-		    if ($element.is('input') || $element.is('select')) {
-		    	//inputs and selects have values, not text 
-		        return $element.val();
-		    }else{
-				return $element.text();
-			}
-		});
-		
-		return values;
+jQuery.fn.calc.FormulaTool = (function () {
+
+    var selectorPattern = /\$\{.*?\}/g;
+
+    function formulaWrapper($element, formula) {
+        return function () {
+            setContent($element, unescape(evalFormula(formula)));
+        };
     }
-    
-    function replaceSelectors(formula) {
+
+    function functionWrapper($element, funktion, elementsArr) {
+
+        return function () {
+            //Map the array of elements to their corresponding values
+            var values = elementsArr.map(function ($element) {
+                return getContents($element);
+            });
+
+            //Apply these values as arguments on the supplied funktion    
+            setContent($element, unescape(funktion.apply(this, values)));
+        };
+    }
+
+    function resolveSelectors(formula) {
+        //split into selectors and parts of the formula
+        //that lie between 
         var selectors = getSelectors(formula);
+        var parts = formula.split(selectorPattern);
 
-        if (selectors.length === 0) {
-            return formula;
+        //two pass selector resolution that avoids 
+        //us having to resolve the same selector
+        //multiple times:
+        //map selectors to their resolved values
+        var selectorsToValues = {};
+        _(selectors).chain().uniq().map(function (selector) {
+            selectorsToValues[selector] = resolveSelector(selector);
+        });
+
+        //resolve every selector
+        var resolvedSelectors = _(selectors).map(function (selector) {
+            return selectorsToValues[selector];
+        });
+
+        //zip it back together
+        return _(parts).chain().zip(resolvedSelectors).flatten().value().join('');
+    }
+
+    function resolveSelector(selector) {
+
+        var resolvedSelector = _(getContents(jQuery(selector))).map(autoParse);
+
+        if (isPluralSelector(selector)) {
+            //wrap array inside '[]' brackets
+            //so that they can evaluated as an array
+            return '[' + resolvedSelector + ']';
+        } else {
+            //no wrapping
+            return resolvedSelector[0];
         }
+    }
 
-        _(selectors).each(function(selector) {
-            formula = replaceSelector(formula, selector);
+    function getContents($elements) {
+
+        var values = _($elements).map(function (element) {
+            var $element = $(element);
+
+            var value = null;
+
+            if ($element.is('input') || $element.is('select')) {
+                //inputs and selects have values, not text 
+                return $element.val();
+            } else {
+                return $element.text();
+            }
         });
 
-        return formula;
-    }
-
-    function replaceSelector(formula, selector) {
-    
-		var values = _(resolve(jQuery(selector))).map(function(value){
-			if (/^[0-9]+$/.test(value)){
-				//String is functionOrFormula number.
-				//convert strings 
-				//containing only numbers
-				//to floats
-				return parseFloat(value);
-			}else{
-				//Escaping any user entered strings 
-				//as they could be interpreted as code
-				//and break the function
-				return '"' + escape(value) + '"';
-			}
-		});
-		
-    	if(isPluralSelector(selector)){
-			//wrap array inside '[]' brackets
-			//so that they can evaluated as an array
-		    return formula.replace('${' + selector + '}', '[' + values + ']');
-	    }else{
-	    	//no wrapping
-	    	return formula.replace('${' + selector + '}', values[0]);
-	    }
-    }
-
-    function resolveAndEvalFormula(formula) {
-        var js = replaceSelectors(formula);
-        return eval(js);
-    }
-
-    function getSelectors(formula) {
-        return _(formula.match(/\$\{.*?\}/g)).map(function(sigil) {
-            return sigil.match(/\$\{(.*?)\}/)[1];
-        });
-    }
-    
-    function isPluralSelector(selector){
-    	//Selectors that could return more than
-    	//one elements are plural selectors e.g. class selectors
-    	//Conversly selectors that return individual
-    	//elements are singular selectors, e.g. id selectors
-    	
-    	if (selector.indexOf(",") > 0){
-    		//multiple selectors is certainly plural
-    		return true;
-    	}
-		
-		var filterPattern = new RegExp(/((\:last)|(\:first)|(\:eq\([0-9]+\))|(\#[a-z][a-z0-9]*))$/)
-    	
-    	if (filterPattern.test(selector) === true){
-    		//A selector ending in #id will only return 
-    		//a singular element 
-    		//:first, :last, and eq(n) are filters that
-    		//only ever return one element, therefore
-    		//they are considered singular
-    		return false;
-    	}
-    	
-    	//any other case is currently handled as plural
-    	return true;
+        return values;
     }
 
     function setContent($element, content) {
-    
+
         if ($element.is('input')) {
             $element.val(content);
         } else {
@@ -209,39 +201,74 @@ jQuery.fn.calc.FormulaTool = (function() {
         }
     }
 
-    function formulaWrapper($element, formula) {
-        return function() {
-            setContent($element, unescape(resolveAndEvalFormula(formula)));
-        };
+    function autoParse(value) {
+        if (/^[0-9]+$/.test(value)) {
+            //String is functionOrFormula number.
+            //convert strings 
+            //containing only numbers
+            //to floats
+            return parseFloat(value);
+        } else {
+            //Escaping any user entered strings 
+            //as they could be interpreted as code
+            //and break the function
+            return '"' + escape(value) + '"';
+        }
     }
 
-    function functionWrapper($element, funktion, elementsArr) {
-   
-        return function() {
-            //Map the array of elements to their corresponding values
-            var values = elementsArr.map(function($element) {
-                return resolve($element);
-            });
-            
-            //Apply these values as arguments on the supplied funktion    
-            setContent($element, unescape(funktion.apply(this, values)));
-        };
+    function evalFormula(formula) {
+        var js = resolveSelectors(formula);
+        return eval(js);
     }
-    
-    function tests(){
-    	
-    	fireunit.ok( isPluralSelector('#not, #singular') === true, "',' is always plural" );
- 	    fireunit.ok( isPluralSelector('td') === true, "ends with a tag" );
- 	    fireunit.ok( isPluralSelector('#this #is #not singular') === true, "ends with a tag" );
-     	fireunit.ok( isPluralSelector('#totally #singular') === false, "ends with an ID" );
-     	fireunit.ok( isPluralSelector('#singular') === false, "ends with an ID" );
- 	    fireunit.ok( isPluralSelector('this is #singular') === false, "ends with an ID" );
- 	    fireunit.ok( isPluralSelector('this is #singular') === false, "ends with an ID" );
- 	    fireunit.ok( isPluralSelector('.this is singular:last') === false, "':last' returns a single element" );
- 	    fireunit.ok( isPluralSelector('.this is singular:first') === false, "':first' returns a single element" );
-        fireunit.ok( isPluralSelector('.this is singular:eq(0)') === false, "'eq()' returns a single element" );
- 	    
-    	fireunit.testDone();
+
+    function getSelectors(formula) {
+
+        var innerSelectorPattern = /\$\{(.*?)\}/;
+
+        return _(formula.match(selectorPattern)).map(function (sigil) {
+            return sigil.match(innerSelectorPattern)[1];
+        });
+    }
+
+    function isPluralSelector(selector) {
+        //Selectors that could return more than
+        //one elements are plural selectors e.g. class selectors
+        //Conversly selectors that return individual
+        //elements are singular selectors, e.g. id selectors
+        if (selector.indexOf(",") > 0) {
+            //multiple selectors is certainly plural
+            return true;
+        }
+
+        var filterPattern = /((\:last)|(\:first)|(\:eq\([0-9]+\))|(\#[a-z][a-z0-9]*))$/;
+
+        if (filterPattern.test(selector) === true) {
+            //A selector ending in #id will only return 
+            //a singular element 
+            //:first, :last, and eq(n) are filters that
+            //only ever return one element, therefore
+            //they are considered singular
+            return false;
+        }
+
+        //any other case is currently handled as plural
+        return true;
+    }
+
+    function tests() {
+
+        fireunit.ok(isPluralSelector('#not, #singular') === true, "',' is always plural");
+        fireunit.ok(isPluralSelector('td') === true, "ends with a tag");
+        fireunit.ok(isPluralSelector('#this #is #not singular') === true, "ends with a tag");
+        fireunit.ok(isPluralSelector('#totally #singular') === false, "ends with an ID");
+        fireunit.ok(isPluralSelector('#singular') === false, "ends with an ID");
+        fireunit.ok(isPluralSelector('this is #singular') === false, "ends with an ID");
+        fireunit.ok(isPluralSelector('this is #singular') === false, "ends with an ID");
+        fireunit.ok(isPluralSelector('.this is singular:last') === false, "':last' returns a single element");
+        fireunit.ok(isPluralSelector('.this is singular:first') === false, "':first' returns a single element");
+        fireunit.ok(isPluralSelector('.this is singular:eq(0)') === false, "'eq()' returns a single element");
+
+        fireunit.testDone();
     }
 
     return {
